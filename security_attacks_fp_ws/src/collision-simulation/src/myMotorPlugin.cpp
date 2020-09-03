5100d2cf3080ea6aca31ae053053bda9e8cc8c5a
Julian9619 <69086557+Julian9619@users.noreply.github.com>
2020-09-03 21:37:08 +0200

    added new plugins

---
+++ security_attacks_fp_ws/src/collision-simulation/src/myMotorPlugin.cpp
@@ -0,0 +1,179 @@
+#include <string>
+#include <gazebo/gazebo.hh>
+#include <gazebo/sensors/sensors.hh>
+#include <gazebo/common/Plugin.hh>
+#include <ros/ros.h>
+#include <std_msgs/Int8MultiArray.h>
+#include "ros/callback_queue.h"
+#include "ros/subscribe_options.h"
+#include <gazebo/physics/physics.hh>
+#include <thread>
+#include <unistd.h>
+
+namespace gazebo
+{
+    class MotorPlugin : public ModelPlugin
+    {
+        public:MotorPlugin() : ModelPlugin(){
+
+        }
+
+        /// \brief Pointer to the model
+        private: physics::ModelPtr model;
+
+        /// \brief Connection that maintains a link between the contact model's
+        /// updated signal and the OnUpdate callback.
+        private: event::ConnectionPtr updateConnection;
+
+        private: bool collisionDetected;
+
+        private: std::unique_ptr<ros::NodeHandle> rosNode;
+
+        private: ros::NodeHandle n;
+
+        private: ros::Publisher sender_pub;
+
+        private: ros::Subscriber rosSub;
+        
+        private: ros::CallbackQueue rosQueue;
+        
+        private: std::thread rosQueueThread;
+        private: int itCounter;
+
+        private: int customer;
+
+        private: int targetCoordinate;
+
+        private: int order;
+
+        private: std_msgs::Int8MultiArray own_msg;
+        /* 
+        Dies ist die Nachricht, die an OMNET++ gesendet wird. Beachte:
+        msg.data[0] = 'ID (die ID entspricht dem Topic mit dem ein CAN-Node Nachrichten pubished)'
+        msg.data[1...] = 'Daten die über CAN versendet werden sollen (nur Int erlaubt!!!!)'
+        */
+
+
+
+        void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+        {
+            this->model = _model;
+
+            this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+            std::bind(&MotorPlugin::OnUpdate, this));
+
+            //initialisiere ROS
+            if (!ros::isInitialized())
+            {
+                int argc = 0;
+                char** argv = NULL;
+                ros::init(argc, argv, "Car", ros::init_options::NoSigintHandler |
+                                        ros::init_options::AnonymousName);
+            }
+
+            rosNode.reset(new ros::NodeHandle("Car"));
+            ros::SubscribeOptions so = ros::SubscribeOptions::create<std_msgs::Int8MultiArray>(
+                "/to_motor", 1,
+                boost::bind(&MotorPlugin::OnRosMsg, this, _1), 
+                ros::VoidPtr(),&rosQueue);
+
+            rosSub = rosNode->subscribe(so);
+            rosQueueThread =std::thread(std::bind(&MotorPlugin::QueueThread, this));
+
+            sender_pub = n.advertise<std_msgs::Int8MultiArray>("from_motor", 1000);
+
+            itCounter = 1000;
+
+            customer = 2;
+
+            order = 1;
+
+            targetCoordinate = 0;  
+
+            ROS_WARN("PLugin loaded");        
+
+        }//end of load
+
+        void start() {
+            this->model->GetJoint("left_wheel_hinge")->SetVelocity(0, -3.0);
+            this->model->GetJoint("right_wheel_hinge")->SetVelocity(0, -3.0);
+        }
+
+        void stop() {
+            this->model->GetJoint("left_wheel_hinge")->SetVelocity(0, 0);
+            this->model->GetJoint("right_wheel_hinge")->SetVelocity(0, 0);
+        }
+
+        void makeOrder(int num) {
+            
+        }
+
+        int getX() {
+            int x,y,z;
+            gazebo::math::Pose pose;     
+            pose = this->model->GetWorldPose();
+            math::Vector3 v(0, 0, 0);
+            v = pose.pos;
+            x = v.x; // x coordinate
+            y = v.y; // y coordinate
+            z = v.z; // z coordinate
+            //ROS_WARN("car: %d, %d, %d", x, y, z);
+            return x;
+        }
+
+        void OnUpdate()
+        {   
+            if(this->getX()>=targetCoordinate) {
+                this->stop();
+            }
+            else{
+                start();
+            }
+
+/*
+            if (this->customer == 0) {
+                this->stop();
+            }
+            else if (this->customer == 1) {
+                if (this->getX() >= 3) {
+                    this->stop();
+                }
+                else {
+                    this->start();
+                }
+            }
+            else if (this->customer == 2) {
+                if (this->getX() >= 7) {
+                    this->stop();
+                }
+                else {
+                    this->start();
+                }
+            }
+*/
+        }//end of update
+
+        
+        public: void OnRosMsg(const std_msgs::Int8MultiArrayConstPtr &msg) 
+        {
+            if(msg->data[0]==1){
+                targetCoordinate = msg->data[1];
+            }
+        }
+
+        
+        
+        // ROS helper function that processes messages
+        private: void QueueThread() 
+        {
+            static const double timeout = 0.1;
+            while (rosNode->ok()) 
+            {
+                rosQueue.callAvailable(ros::WallDuration(timeout));
+            }
+        }
+
+        
+    };
+    GZ_REGISTER_MODEL_PLUGIN(MotorPlugin)
+}
---
+++ security_attacks_fp_ws/src/collision-simulation/src/myRecieverPlugin.cpp
@@ -0,0 +1,123 @@
+#include <string>
+#include <gazebo/gazebo.hh>
+#include <gazebo/sensors/sensors.hh>
+#include <gazebo/common/Plugin.hh>
+#include <ros/ros.h>
+#include <std_msgs/Int8MultiArray.h>
+#include "ros/callback_queue.h"
+#include "ros/subscribe_options.h"
+#include <gazebo/physics/physics.hh>
+#include <thread>
+#include <unistd.h>
+
+namespace gazebo
+{
+    class RecieverPlugin : public ModelPlugin
+    {
+        public:RecieverPlugin() : ModelPlugin(){
+
+        }
+
+        /// \brief Pointer to the model
+        private: physics::ModelPtr model;
+
+        /// \brief Connection that maintains a link between the contact model's
+        /// updated signal and the OnUpdate callback.
+        private: event::ConnectionPtr updateConnection;
+
+        private: bool collisionDetected;
+
+        private: std::unique_ptr<ros::NodeHandle> rosNode;
+
+        private: ros::NodeHandle n;
+
+        private: ros::Publisher sender_pub;
+
+        private: ros::Subscriber rosSub;
+        
+        private: ros::CallbackQueue rosQueue;
+        
+        private: std::thread rosQueueThread;
+        private: int itCounter;
+
+        private: std_msgs::Int8MultiArray own_msg;
+        /* 
+        Dies ist die Nachricht, die an OMNET++ gesendet wird. Beachte:
+        msg.data[0] = 'ID (die ID entspricht dem Topic mit dem ein CAN-Node Nachrichten pubished)'
+        msg.data[1...] = 'Daten die über CAN versendet werden sollen (nur Int erlaubt!!!!)'
+        */
+
+
+
+        void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+        {
+            this->model = _model;
+
+            this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+            std::bind(&RecieverPlugin::OnUpdate, this));
+
+            //initialisiere ROS
+            if (!ros::isInitialized())
+            {
+                int argc = 0;
+                char** argv = NULL;
+                ros::init(argc, argv, "dein_Client_Name_TODO", ros::init_options::NoSigintHandler |
+                                        ros::init_options::AnonymousName);
+            }
+
+            rosNode.reset(new ros::NodeHandle("dein_Client_Name_TODO"));
+            ros::SubscribeOptions so = ros::SubscribeOptions::create<std_msgs::Int8MultiArray>(
+                "/from_postbox3", 1,
+                boost::bind(&RecieverPlugin::OnRosMsg, this, _1), 
+                ros::VoidPtr(),&rosQueue);
+
+            rosSub = rosNode->subscribe(so);
+            rosQueueThread =std::thread(std::bind(&RecieverPlugin::QueueThread, this));
+
+            sender_pub = n.advertise<std_msgs::Int8MultiArray>("from_reciever", 1000);
+            
+            //Topic für OMNET++ der Nachricht uebergeben (hier z.B. 1)
+            //zwei mal push_back um die data Eintraege zu erzeugen. Ansonsten entstehet ein Speicher-Fehler
+            
+            ///////////TODO////////////TODO/////////////
+            //own_msg.data.push_back(2); //CAN-Topic
+            //own_msg.data.push_back(0); //initalisialer Wert für Nachricht (in diesem fall -1 == request)
+            ///////////TODO////////////TODO////////////
+
+            ROS_WARN("Reciever loaded");
+        }//end of load
+
+        void OnUpdate()
+        {          
+
+        }//end of update
+
+        
+        public: void OnRosMsg(const std_msgs::Int8MultiArrayConstPtr &msg) 
+        {
+            std_msgs::Int8MultiArray recieverMsg;
+            recieverMsg.data.push_back(1); //CAN-messagetype
+            recieverMsg.data.push_back(0);
+            if(msg->data[0]==1){
+                recieverMsg.data[1] = 3; //Postbox-Index
+            }
+            if(msg->data[0] == 2){
+                recieverMsg.data[1] = 7;
+            }
+            sender_pub.publish(recieverMsg);
+        }
+
+        // ROS helper function that processes messages
+        private: void QueueThread() 
+        {
+            static const double timeout = 0.1;
+            while (rosNode->ok()) 
+            {
+                rosQueue.callAvailable(ros::WallDuration(timeout));
+            }
+        }
+
+        
+    };
+    GZ_REGISTER_MODEL_PLUGIN(RecieverPlugin)
+}
