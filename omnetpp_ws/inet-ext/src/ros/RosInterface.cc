6d9be68382bf12022ad4d4ee1e755a00faa262fd
fp <fp@fp-VirtualBox>
2020-09-08 18:48:05 +0200

    changed omnetpp.ini

--- omnetpp_ws/inet-ext/src/ros/RosInterface.cc
+++ omnetpp_ws/inet-ext/src/ros/RosInterface.cc
@@ -24,21 +24,21 @@ void RosInterface::handleMessage(cMessage *msg) {
         scheduleAt(simTime()+delay, msg); //TOTO simTime
 
         //////////////////////////////
-        std_msgs::Int8MultiArray msgInt8;
-        std_msgs::Int8MultiArray *rosMsg = &msgInt8;
-        rosMsg->data.push_back(1);
-
-        auto pkt = new Packet;
-        auto data = makeShared<BytesChunk>();
-        int d0 = rosMsg->data[0];
-        data->setBytes({d0});
-        pkt->insertAtBack(data);
-        sendDirect(pkt->dup(), gates.at(0));
-        sendDirect(pkt->dup(), gates.at(0));
-        sendDirect(pkt->dup(), gates.at(1));
-        sendDirect(pkt->dup(), gates.at(1));
-        sendDirect(pkt, gates.at(1));
-        EV_INFO << data->str() << "\n";
+//        std_msgs::Int8MultiArray msgInt8;
+//        std_msgs::Int8MultiArray *rosMsg = &msgInt8;
+//        rosMsg->data.push_back(1);
+//
+//        auto pkt = new Packet;
+//        auto data = makeShared<BytesChunk>();
+//        int d0 = rosMsg->data[0];
+//        data->setBytes({d0});
+//        pkt->insertAtBack(data);
+//        sendDirect(pkt->dup(), gates.at(0));
+//        sendDirect(pkt->dup(), gates.at(0));
+//        sendDirect(pkt->dup(), gates.at(1));
+//        sendDirect(pkt->dup(), gates.at(1));
+//        sendDirect(pkt, gates.at(1));
+//        EV_INFO << data->str() << "\n";
         //////////////////////////////
     }
 }
